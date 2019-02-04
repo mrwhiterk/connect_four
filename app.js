92b5eb74f6301571d5b60454e474002b01d03ae8
Ryan White <mrwhiterk@gmail.com>
2019-02-04 16:02:10 -0500

    got pieces stacking

--- app.js
+++ app.js
@@ -1,35 +1,97 @@
 const cells = document.querySelectorAll('.cell');
-let currentPlayer = 1;
+const resetBtn = document.querySelector('.reset');
+let isPlayerOne = true;
 
-let resetBtn = document.querySelector('.reset');
+let column1 = [];
+
+for (var i = 0; i < 42; i++) {
+  if (i % 7 == 0) column1.push(i);
+}
+
+let column2 = column1.map(x => x + 1)
+let column3 = column2.map(x => x + 1)
+let column4 = column3.map(x => x + 1)
+let column5 = column4.map(x => x + 1)
+let column6 = column5.map(x => x + 1)
+let column7 = column6.map(x => x + 1)
+
+var divArray = document.querySelectorAll(".cell");
+
+// how close to bottom
+var column1position = 0
+var column2position = 0
+var column3position = 0
+var column4position = 0
+var column5position = 0
+var column6position = 0
+var column7position = 0
 
 for (let i = 0; i < cells.length; i++) {
-
+  cells[i].innerText = i
   cells[i].addEventListener('click', (evt) => {
 
-    if (currentPlayer === 1) {
-      // evt.target.style.backgroundColor = "red";
-      evt.target.style.color = "red";
-      evt.target.innerText = "X";
-      currentPlayer = 2;
+
+    var match = findColumn(evt.target);
+
+    if (isPlayerOne) {
+      match.style.backgroundColor = "red";
     } else {
-      // evt.target.style.backgroundColor = "green";
-      evt.target.style.color = "green";
-      evt.target.innerText = "O";
-      currentPlayer = 1;
+      match.style.backgroundColor = "green";
     }
-    // disable click
-    evt.target.style.pointerEvents = "none";
-    evt.target.style.fontSize = "120px";
+    isPlayerOne = !isPlayerOne;
+    column1bottom.style.pointerEvents = "none";
+
 
   });
 }
-console.log(resetBtn);
+
 resetBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
   for (let i = 0; i < cells.length; i++) {
-    cells[i].innerHTML = "";
+    cells[i].style.backgroundColor = "lightblue";
     cells[i].style.pointerEvents = "auto";
-    currentPlayer = 1;
+    isPlayerOne = true;
   }
 });
+
+
+
+function findColumn(item) {
+  //column1
+  if (column1.indexOf(parseInt(item.innerText)) > -1) {
+    var lastCol1 = column1[column1.length - 1];
+    return divArray[lastCol1 - (column1position++ * 7)];
+
+  }
+  //column2
+  if (column2.indexOf(parseInt(item.innerText)) > -1) {
+    var lastCol1 = column2[column2.length - 1];
+    return divArray[lastCol1 - (column2position++ * 7)];
+  }
+  //column3
+  if (column3.indexOf(parseInt(item.innerText)) > -1) {
+    var lastCol1 = column3[column3.length - 1];
+    return divArray[lastCol1 - (column3position++ * 7)];
+  }
+  //column4
+  if (column4.indexOf(parseInt(item.innerText)) > -1) {
+    var lastCol1 = column4[column4.length - 1];
+    return divArray[lastCol1 - (column4position++ * 7)];
+  }
+  //column5
+  if (column5.indexOf(parseInt(item.innerText)) > -1) {
+    var lastCol1 = column5[column5.length - 1];
+    return divArray[lastCol1 - (column5position++ * 7)];
+  }
+  //column6
+  if (column6.indexOf(parseInt(item.innerText)) > -1) {
+    var lastCol1 = column6[column6.length - 1];
+    return divArray[lastCol1 - (column6position++ * 7)];
+
+  }
+  //column7
+  if (column7.indexOf(parseInt(item.innerText)) > -1) {
+    var lastCol1 = column7[column7.length - 1];
+    return divArray[lastCol1 - (column7position++ * 7)];
+  }
+}
